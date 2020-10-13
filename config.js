7c973936fa939e1d85460f1288006987290388b9
lucifer-web-tech <69235702+lucifer-web-tech@users.noreply.github.com>
2020-10-13 07:58:45 +0530

    Update config.js
    changed the DB

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 const str = {
-    "connectionString": "mongodb+srv://interview:welcome2byteridge@byteridge-hdrl6.mongodb.net/interview",
+    "connectionString": "mongodb+srv://interview:welcome2byteridge@byteridge-hdrl6.mongodb.net/interviewDB",
     "secret": "JBJYBJ23jkj232kALKMLAK"
 }
 
